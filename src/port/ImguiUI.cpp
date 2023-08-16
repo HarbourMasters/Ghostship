e660af40c1ea1c9c4a09c4868ec00281883c8597
KiritoDv <kiritodev01@gmail.com>
2023-08-16 09:27:04 -0600

    Bump LUS and implemented Menu Bar

--- src/port/ImguiUI.cpp
+++ src/port/ImguiUI.cpp
@@ -8,6 +8,7 @@
 #include <Fast3D/gfx_pc.h>
 
 namespace GameUI {
+std::shared_ptr<GameMenuBar> mGameMenuBar;
 std::shared_ptr<LUS::GuiWindow> mConsoleWindow;
 std::shared_ptr<LUS::GuiWindow> mStatsWindow;
 std::shared_ptr<LUS::GuiWindow> mInputEditorWindow;
@@ -15,6 +16,8 @@ std::shared_ptr<LUS::GuiWindow> mInputEditorWindow;
 void SetupGuiElements() {
     auto gui = LUS::Context::GetInstance()->GetWindow()->GetGui();
 
+    mGameMenuBar = std::make_shared<GameMenuBar>("gOpenMenuBar", CVarGetInteger("gOpenMenuBar", 0));
+    gui->SetMenuBar(mGameMenuBar);
     mStatsWindow = gui->GetGuiWindow("Stats");
     if (mStatsWindow == nullptr) {
         SPDLOG_ERROR("Could not find stats window");
@@ -30,7 +33,6 @@ void SetupGuiElements() {
         SPDLOG_ERROR("Could not find input editor window");
         return;
     }
-    mInputEditorWindow->ToggleVisibility();
 }
 
 void Destroy() {
@@ -38,4 +40,19 @@ void Destroy() {
     mStatsWindow = nullptr;
     mInputEditorWindow = nullptr;
 }
+}
+
+void GameMenuBar::DrawElement() {
+    if(ImGui::BeginMenuBar()){
+        if(ImGui::BeginMenu("Settings")){
+            if (GameUI::mInputEditorWindow) {
+                if (ImGui::Button("Controller Mapping", ImVec2 (-1.0f, 0.0f))) {
+                    GameUI::mInputEditorWindow->ToggleVisibility();
+                }
+            }
+            LUS::Context::GetInstance()->GetWindow()->GetGui()->GetGameOverlay()->DrawSettings();
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
 }
