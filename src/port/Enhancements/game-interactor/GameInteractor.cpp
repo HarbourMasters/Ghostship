38646ba84b7567c4a66464a7c3c2ac3254dc800f
inspectredc <inspectredc@gmail.com>
2023-10-07 00:39:43 +0100

    pause state helper example

--- src/port/Enhancements/game-interactor/GameInteractor.cpp
+++ src/port/Enhancements/game-interactor/GameInteractor.cpp
@@ -1,12 +1,16 @@
 #include "GameInteractor.h"
 
-bool GameInteractor::IsSaveLoaded() {
-    // todo
-    return false;
-}
+#include "game/level_update.h"
 
+extern int16_t sCurrPlayMode;
+
+// Taken from game/level_update.c
+#define PLAY_MODE_NORMAL 0
+#define PLAY_MODE_PAUSED 2
+#define PLAY_MODE_CHANGE_AREA 3
+#define PLAY_MODE_CHANGE_LEVEL 4
+#define PLAY_MODE_FRAME_ADVANCE 5
 
 bool GameInteractor::IsGameplayPaused() {
-    // todo
-    return false;
+    return sCurrPlayMode == PLAY_MODE_PAUSED;
 }
