10915c156b41bc8ba3eed3063d9f22c1f03a6818
Garrett Cox <garrettjcox@gmail.com>
2023-10-16 12:25:54 -0500

    Adjustment to infinite cap timer cheat

--- src/port/Enhancements/mods.cpp
+++ src/port/Enhancements/mods.cpp
@@ -3,11 +3,12 @@
 #include "game-interactor/GameInteractor.h"
 
 #include "game/level_update.h"
+#include "include/sm64.h"
 extern MarioState* gMarioState;
 
 #define MARIO_HEALTH_MAX 0x880
 
-void RegisterInfiniteHealth() {
+void RegisterCheatsHooks() {
     GameInteractor::Instance->RegisterGameHook<GameInteractor::OnHealthChange>([](int16_t health) {
         if (!gMarioState) return;
 
@@ -15,20 +16,21 @@ void RegisterInfiniteHealth() {
             gMarioState->health = MARIO_HEALTH_MAX;
         }
     });
-}
 
-void RegisterInfiniteLives() {
-    // There may be a better hook that can be made, rather than doing this every frame
     GameInteractor::Instance->RegisterGameHook<GameInteractor::OnGameFrameUpdate>([]() {
         if (!gMarioState) return;
 
+        // There may be a better hook that can be made, rather than doing this every frame
         if (CVarGetInteger("gInfiniteLives", 0) != 0) {
             gMarioState->numLives = 100;
+        }
+
+        if (CVarGetInteger("gInfiniteCapTimer", 0) && (gMarioState->flags & MARIO_CAP_ON_HEAD) && (gMarioState->flags & MARIO_SPECIAL_CAPS)) {
+            gMarioState->capTimer = 1200;
         }
     });
 }
 
 void InitMods() {
-    RegisterInfiniteHealth();
-    RegisterInfiniteLives();
+    RegisterCheatsHooks();
 }
