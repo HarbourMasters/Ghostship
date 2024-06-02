f492295c2a29a21c2e726e5459b61c8e3600e90f
KiritoDv <KiritoDv>
2024-06-01 20:10:35 -0600

    Fixed skyboxes

--- include/skybox_table.h
+++ include/skybox_table.h
@@ -1,32 +1,32 @@
 #pragma once
 
 #include "align_asset_macro.h"
-#define dgSkyboxBbh "__OTR__textures/skyboxes/bbh"
+#define dgSkyboxBbh "__OTR__textures/skyboxes/bbh/bbh"
 static const ALIGN_ASSET(2) char gSkyboxBbh[] = dgSkyboxBbh;
 
-#define dgSkyboxBitdw "__OTR__textures/skyboxes/bidw"
+#define dgSkyboxBitdw "__OTR__textures/skyboxes/bidw/bidw"
 static const ALIGN_ASSET(2) char gSkyboxBitdw[] = dgSkyboxBitdw;
 
-#define dgSkyboxBitfs "__OTR__textures/skyboxes/bitfs"
+#define dgSkyboxBitfs "__OTR__textures/skyboxes/bitfs/bitfs"
 static const ALIGN_ASSET(2) char gSkyboxBitfs[] = dgSkyboxBitfs;
 
-#define dgSkyboxBits "__OTR__textures/skyboxes/bits"
+#define dgSkyboxBits "__OTR__textures/skyboxes/bits/bits"
 static const ALIGN_ASSET(2) char gSkyboxBits[] = dgSkyboxBits;
 
-#define dgSkyboxCcm "__OTR__textures/skyboxes/ccm"
+#define dgSkyboxCcm "__OTR__textures/skyboxes/ccm/ccm"
 static const ALIGN_ASSET(2) char gSkyboxCcm[] = dgSkyboxCcm;
 
-#define dgSkyboxCloudFloor "__OTR__textures/skyboxes/cloud_floor"
+#define dgSkyboxCloudFloor "__OTR__textures/skyboxes/cloud_floor/cloud_floor"
 static const ALIGN_ASSET(2) char gSkyboxCloudFloor[] = dgSkyboxCloudFloor;
 
-#define dgSkyboxClouds "__OTR__textures/skyboxes/clouds"
+#define dgSkyboxClouds "__OTR__textures/skyboxes/clouds/clouds"
 static const ALIGN_ASSET(2) char gSkyboxClouds[] = dgSkyboxClouds;
 
-#define dgSkyboxSsl "__OTR__textures/skyboxes/ssl"
+#define dgSkyboxSsl "__OTR__textures/skyboxes/ssl/ssl"
 static const ALIGN_ASSET(2) char gSkyboxSsl[] = dgSkyboxSsl;
 
-#define dgSkyboxWater "__OTR__textures/skyboxes/water"
+#define dgSkyboxWater "__OTR__textures/skyboxes/water/water"
 static const ALIGN_ASSET(2) char gSkyboxWater[] = dgSkyboxWater;
 
-#define dgSkyboxWdw "__OTR__textures/skyboxes/wdw"
+#define dgSkyboxWdw "__OTR__textures/skyboxes/wdw/wdw"
 static const ALIGN_ASSET(2) char gSkyboxWdw[] = dgSkyboxWdw;
