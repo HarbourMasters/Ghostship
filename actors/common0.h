a54aed50e0e3200fdbfa7dc5f6169cab1b9dd77d
KiritoDv <KiritoDv>
2024-06-10 21:21:52 -0600

    Ported most of common0 to otr

--- actors/common0.h
+++ actors/common0.h
@@ -4,12 +4,7 @@
 #include "types.h"
 
 // amp
-extern const GeoLayout dAmpGeo[];
-extern const Gfx dAmpElectricityDl[];
-extern const Gfx dAmpEyeDl[];
-extern const Gfx dAmpMouthDl[];
-extern const Gfx dAmpBodyDl[];
-extern const struct Animation *const dAmpAnimsList[];
+#include "assets/actors/amp.h"
 
 // blue_coin_switch
 #include "assets/actors/blue_coin_switch.h"
@@ -37,123 +32,33 @@ extern const GeoLayout bowling_ball_track_geo[];
 #include "assets/actors/checkerboard_platform.h"
 
 // chuckya
-extern const GeoLayout chuckya_geo[];
-extern const Gfx chuckya_seg8_dl_08009FF8[];
-extern const Gfx chuckya_seg8_dl_0800A030[];
-extern const Gfx chuckya_seg8_dl_0800A068[];
-extern const Gfx chuckya_seg8_dl_0800A160[];
-extern const Gfx chuckya_seg8_dl_0800A198[];
-extern const Gfx chuckya_seg8_dl_0800A1D0[];
-extern const Gfx chuckya_seg8_dl_0800A2C8[];
-extern const Gfx chuckya_seg8_dl_0800A300[];
-extern const Gfx chuckya_seg8_dl_0800A338[];
-extern const Gfx chuckya_seg8_dl_0800A3F0[];
-extern const Gfx chuckya_seg8_dl_0800A428[];
-extern const Gfx chuckya_seg8_dl_0800A4D8[];
-extern const Gfx chuckya_seg8_dl_0800A510[];
-extern const Gfx chuckya_seg8_dl_0800A5C0[];
-extern const Gfx chuckya_seg8_dl_0800A5F8[];
-extern const Gfx chuckya_seg8_dl_0800A700[];
-extern const Gfx chuckya_seg8_dl_0800A758[];
-extern const Gfx chuckya_seg8_dl_0800A870[];
-extern const Gfx chuckya_seg8_dl_0800A8D0[];
-extern const Gfx chuckya_seg8_dl_0800A958[];
-extern const Gfx chuckya_seg8_dl_0800A998[];
-extern const Gfx chuckya_seg8_dl_0800AB70[];
-extern const Gfx chuckya_seg8_dl_0800ABE8[];
-extern const struct Animation *const chuckya_seg8_anims_0800C070[];
+#include "assets/actors/chuckya.h"
 
 // exclamation_box
-extern const GeoLayout exclamation_box_geo[];
-extern const Gfx exclamation_box_seg8_dl_08018FA8[];
-extern const Gfx exclamation_box_seg8_dl_08019008[];
-extern const Gfx exclamation_box_seg8_dl_08019058[];
-extern const Gfx exclamation_box_seg8_dl_08019220[];
-extern const Gfx exclamation_box_seg8_dl_08019280[];
-extern const Gfx exclamation_box_seg8_dl_080192D0[];
-extern const Gfx exclamation_box_seg8_dl_08019318[];
-extern const Gfx exclamation_box_seg8_dl_08019378[];
-extern const Gfx exclamation_box_seg8_dl_080193D8[];
-extern const Gfx exclamation_box_seg8_dl_08019438[];
+#include "assets/actors/exclamation_box.h"
 
 // exclamation_box_outline
-extern const GeoLayout exclamation_box_outline_geo[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024EB8[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024F30[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024F58[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024F88[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024FA8[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024FC8[];
-extern const Gfx exclamation_box_outline_seg8_dl_08024FE8[];
-extern const Gfx exclamation_box_outline_seg8_dl_08025968[];
-extern const Gfx exclamation_box_outline_seg8_dl_080259F8[];
-extern const Gfx exclamation_box_outline_seg8_dl_08025EC0[];
-extern const Gfx exclamation_box_outline_seg8_dl_08025F08[];
-extern const Collision exclamation_box_outline_seg8_collision_08025F78[];
+#include "assets/actors/exclamation_box_outline.h"
 
 // flyguy
-extern const GeoLayout flyguy_geo[];
-extern const Gfx flyguy_seg8_dl_08010840[];
-extern const Gfx flyguy_seg8_dl_08010968[];
-extern const Gfx flyguy_seg8_dl_08010A90[];
-extern const Gfx flyguy_seg8_dl_08010B38[];
-extern const Gfx flyguy_seg8_dl_08010B80[];
-extern const Gfx flyguy_seg8_dl_080113A8[];
-extern const Gfx flyguy_seg8_dl_08011420[];
-extern const Gfx flyguy_seg8_dl_080116D0[];
-extern const Gfx flyguy_seg8_dl_08011710[];
-extern const struct Animation *const flyguy_seg8_anims_08011A64[];
+#include "assets/actors/flyguy.h"
 
 // goomba
-extern const GeoLayout goomba_geo[];
-extern const Gfx goomba_seg8_dl_0801B2E8[];
-extern const Gfx goomba_seg8_dl_0801B560[];
-extern const Gfx goomba_seg8_dl_0801B5A0[];
-extern const Gfx goomba_seg8_dl_0801B5C8[];
-extern const Gfx goomba_seg8_dl_0801B5F0[];
-extern const Gfx goomba_seg8_dl_0801B658[];
-extern const Gfx goomba_seg8_dl_0801B690[];
-extern const Gfx goomba_seg8_dl_0801CE20[];
-extern const Gfx goomba_seg8_dl_0801CF78[];
-extern const Gfx goomba_seg8_dl_0801D0D0[];
-extern const Gfx goomba_seg8_dl_0801D360[];
-extern const Gfx goomba_seg8_dl_0801D760[];
-extern const struct Animation *const goomba_seg8_anims_0801DA4C[];
+#include "assets/actors/goomba.h"
 
 // heart
-extern const GeoLayout heart_geo[];
-extern const Gfx heart_seg8_dl_0800DFE0[];
+#include "assets/actors/heart.h"
 
 // koopa_shell
-extern const GeoLayout koopa_shell_geo[];
-extern const GeoLayout koopa_shell2_geo[];
-extern const GeoLayout koopa_shell3_geo[];
-extern const Gfx koopa_shell_seg8_dl_08027108[];
-extern const Gfx koopa_shell_seg8_dl_08027170[];
-extern const Gfx koopa_shell_seg8_dl_08027258[];
-extern const Gfx koopa_shell_seg8_dl_080273C8[];
-extern const Gfx koopa_shell_seg8_dl_08027420[];
-extern const Gfx koopa_shell_seg8_dl_080288E0[];
-extern const Gfx koopa_shell_seg8_dl_08028978[];
-extern const Gfx koopa_shell_seg8_dl_08028A20[];
-extern const Gfx koopa_shell_seg8_dl_08028B78[];
+#include "assets/actors/koopa_shell.h"
 
 // metal_box
-extern const GeoLayout metal_box_geo[];
-extern const Gfx metal_box_seg8_dl_08024B18[];
-extern const Gfx metal_box_dl[];
-extern const Collision metal_box_seg8_collision_08024C28[];
+#include "assets/actors/metal_box.h"
 
 // purple_switch
-extern const GeoLayout purple_switch_geo[];
-extern const Gfx purple_switch_seg8_dl_0800C668[];
-extern const Gfx purple_switch_seg8_dl_0800C6E0[];
-extern const Gfx purple_switch_seg8_dl_0800C718[];
-extern const Collision purple_switch_seg8_collision_0800C7A8[];
+#include "assets/actors/purple_switch.h"
 
-// test_platform
-extern const GeoLayout test_platform_geo[];
-extern const Gfx unknown_seg8_dl_08026260[];
-extern const Collision unknown_seg8_collision_080262F8[];
+// test_platform - UNUSED Not worth the effort
+// #include "assets/actors/test_platform.h"
 
 #endif
--- actors/common0_geo.c
+++ actors/common0_geo.c
@@ -8,15 +8,4 @@
 #include "common0.h"
 
 // Hmm, this is included earlier.
-#include "test_platform/geo.inc.c"
-#include "amp/geo.inc.c"
-#include "chuckya/geo.inc.c"
-#include "purple_switch/geo.inc.c"
-#include "heart/geo.inc.c"
-#include "flyguy/geo.inc.c"
 #include "bowling_ball/geo.inc.c"
-#include "exclamation_box/geo.inc.c"
-#include "goomba/geo.inc.c"
-#include "metal_box/geo.inc.c"
-#include "exclamation_box_outline/geo.inc.c"
-#include "koopa_shell/geo.inc.c"
