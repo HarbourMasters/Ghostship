e4f8eb7cf1624647e73ccb4377ff718a5c28add8
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2026-01-08 17:52:30 +0100

    Move to event system

--- src/port/hooks/list/PlayerEvent.h
+++ src/port/hooks/list/PlayerEvent.h
@@ -2,12 +2,12 @@
 
 #include "port/hooks/impl/EventSystem.h"
 
-DEFINE_EVENT(PlayerHealthChange, 
+DEFINE_EVENT(PlayerHealthChange,
     struct MarioState* m;
     s32 health;
 );
 
-DEFINE_EVENT(PlayerLivesChange, 
+DEFINE_EVENT(PlayerLivesChange,
     struct MarioState* m;
     s32 lives;
 );
