8b805b1fd60b3457d16216c392158edaae4b3b91
KiritoDv <kiritodev01@gmail.com>
2026-01-09 03:05:53 -0600

    Fixed volume sliders

--- src/audio/load.c
+++ src/audio/load.c
@@ -542,9 +542,9 @@ void audio_init() {
     init_sequence_players();
     gAudioLoadLock = AUDIO_LOCK_NOT_LOADING;
 
-    audio_set_player_volume(SEQ_PLAYER_LEVEL, CVarGetFloat("gSettings.Volume.MainMusic", 1.0f));
-    audio_set_player_volume(SEQ_PLAYER_ENV, CVarGetFloat("gSettings.Volume.Environment", 1.0f));
-    audio_set_player_volume(SEQ_PLAYER_SFX, CVarGetFloat("gSettings.Volume.SFX", 1.0f));
+    audio_set_player_volume(SEQ_PLAYER_LEVEL, CVarGetInteger("gSettings.Volume.MainMusic", 100) / 100.0f);
+    audio_set_player_volume(SEQ_PLAYER_ENV, CVarGetInteger("gSettings.Volume.Environment", 100) / 100.0f);
+    audio_set_player_volume(SEQ_PLAYER_SFX, CVarGetInteger("gSettings.Volume.SFX", 100) / 100.0f);
 
     // Should probably contain the sizes of the data banks, but those aren't
     // easily accessible from here.
