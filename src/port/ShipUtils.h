df93d48cdcc1d8f7894cd6db7d3a7fc6ad584ff0
Caladius <clatini88@gmail.com>
2026-02-03 10:56:37 -0500

    Fix linux build 1

--- src/port/ShipUtils.h
+++ src/port/ShipUtils.h
@@ -16,12 +16,12 @@ uint32_t Ship_Hash(std::string str);
 void LoadGuiTextures();
 extern std::array<const char*, 10> digitList;
 extern std::map<int16_t, std::string> levelIdList;
+extern std::string convertEnumToReadableName(const std::string& input);
 
 extern "C" {
 #endif
 
 bool Ship_IsCStringEmpty(const char* str);
-std::string convertEnumToReadableName(const std::string& input);
 int16_t Ship_GetCourseByLevel(int16_t levelId);
 
 #ifdef __cplusplus
